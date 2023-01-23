83c740bfa1ea4514a186d0b3c2f5b47e0f0dd4a5
scrviy <91909966+scrviy@users.noreply.github.com>
2023-01-23 19:41:38 -0300

    att pokemons

--- Db.cs
+++ Db.cs
@@ -10,9 +10,9 @@ public class PokeDB
 {
     private static List<Pokemon> _pokemons = new List<Pokemon>()
     {
-        new Pokemon { Id = 1, Name = "Ash, seu amigao. " },
-        new Pokemon { Id = 2, Name = "Pikachu, o seu amigo mais pika. " },
-        new Pokemon { Id = 3, Name = "Bonossauro, sua vacina mais efetiva. " }
+        new Pokemon { Id = 1, Name = "Bulbasaur, There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger. " },
+        new Pokemon { Id = 2, Name = "Pikachu, When it is angered, it immediately discharges the energy stored in the pouches in its cheeks. " },
+        new Pokemon { Id = 3, Name = "Charizar, It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames. " }
     };
 
     public static List<Pokemon> GetPokemon()
